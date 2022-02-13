46db46842129317bfa66003279edd5dd0663c5a9
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2022-02-12 22:29:18 -0700

    YCM change bnl-email-tunnel command

--- bnl_email.sh
+++ bnl_email.sh
@@ -2,6 +2,6 @@
 
 (
   if [[ $(echo "$( ps -ef | grep 'mail.rhic.bnl' | grep -v 'grep' | wc -l | bc) > 0" | bc ) == 0 ]]; then
-    ssh -fN -l ycmorales ssh.sdcc.bnl.gov -L 1993:mail.rhic.bnl.gov:993 > /dev/null 2>&1
+    ssh -fNL 1993:mail.rhic.bnl.gov:993 sdcc_gateway &> /dev/null
   fi
 )
